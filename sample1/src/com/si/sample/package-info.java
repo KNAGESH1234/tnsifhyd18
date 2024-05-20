7b0a87053791fb8dba0db22824d5c05140d9beab
KNAGESH1234 <168441449+KNAGESH1234@users.noreply.github.com>
2024-05-20 10:12:47 +0530

    Add files via upload

---
+++ sample1/src/com/si/sample/package-info.java
@@ -0,0 +1,1 @@
+package com.si.sample;
---
+++ sample1/src/com/si/sample/sample1.java
@@ -0,0 +1,16 @@
+package com.si.sample;
+
+public class sample1 {
+	int a=20;
+	static int b=10;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sample1 s1=new sample1();
+		System.out.println(s1.a);
+		System.out.println(sample1.b);
+
+	}
+
+}
